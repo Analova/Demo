f8ebbf2e1e2d31bff396aea562352474524bb072
Ana Nikolova <ani.nik19@gmail.com>
2020-09-07 08:27:24 +0200

    first commit

---
+++ dev/js/index.js
@@ -0,0 +1,8 @@
+import "babel-polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>We have new users now!</h1>,
+  document.getElementById("root")
+);
