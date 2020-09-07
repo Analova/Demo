3d1e4a8d438d28f3ef25c3d1164d9ec7f9c07203
Ana Nikolova <ani.nik19@gmail.com>
2020-09-07 10:28:53 +0200

    Store, Providers,Reducers

---
+++ dev/js/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//require("../../scss/styles.css");
+
+const App = () => {
+  return (
+    <div>
+      <h2>Username List</h2>
+      <hr />
+      <h2>UserDetails</h2>
+    </div>
+  );
+};
+
+export default App;
